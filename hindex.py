93dd5695dd250a5e46640bfeae22294f652506de
Frostbite22 <landoulsiferes@outlook.com>
2022-09-28 22:37:10 +0100

    notebooks

--- hindex.py
+++
@@ -1,7 +0,0 @@
-from scholarly import scholarly
-
-# Retrieve the author's data, fill-in, and print
-search_query = scholarly.search_author('Adel Trabelsi')
-author = next(search_query).fill(sections=['indices'])
-print(author)
-
