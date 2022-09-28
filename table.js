ea9e258d5c7c0efbd2a799e9af0a087ea2055302
Frostbite22 <landoulsiferes@outlook.com>
2022-09-29 00:07:27 +0100

    sort the authors

--- table.js
+++ table.js
@@ -10,12 +10,40 @@ authors_h_index.map((author) =>
     "profile_name" : `${author.profile_name}`,
     "profile_affiliations" : `${author.profile_affiliations}`,
     "profile_interests" : `${author.profile_interests}`,
-    "h_index" : `${author.h_index}`
+    "h_index" : `${author.h_index}`,
   }
   authors_json.push(author_h);
+})
 
+const sorted = Object.entries(authors_json).sort(
+  ([,a],[,b]) =>
+  {
+    return parseInt(a['h_index'])-parseInt(b['h_index']) ;
+  } 
+).reverse()
+
+const res = [];
+let i = 0 ;
+sorted.map((author) => 
+{
+  author[1]['rank'] = i.toString();
+  res.push(author[1]);
+  i++ ;
+})
+
+const final_res = [];
+res.map((author) => 
+{
+  let author_h =
+  {
+    "rank" : `${parseInt(author.rank)+1}`,
+    "profile_name" : `${author.profile_name}`,
+    "profile_affiliations" : `${author.profile_affiliations}`,
+    "profile_interests" : `${author.profile_interests}`,
+    "h_index" : `${author.h_index}`
+  }
+  final_res.push(author_h)
 })
 
 
-console.log(tablemark(authors_json,{ wrapWidth: 20 }),
-);
+console.log(tablemark(final_res,{ wrapWidth: 20 }));
